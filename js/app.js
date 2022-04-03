36a98e352138cbb981401cc63b2d4aed2bc763ce
tranlevienngoc <tranngoc11051996@gmail.com>
2022-04-03 19:08:39 +0700

    fix ui home page ver1

--- js/app.js
+++ js/app.js
@@ -70,7 +70,7 @@ function closeNav() {
 
 const navLinks = document.querySelectorAll(".nav-item");
 navLinks.forEach((l) => {
-  if (window.innerWidth < 992 && !l.classList.contains("dropdown")) {
+  if (window.innerWidth < 1292 && !l.classList.contains("dropdown")) {
     l.addEventListener("click", () => {
       $(".collapse").removeClass("show");
       $(".collapse").addClass("closing");
@@ -93,3 +93,24 @@ $(document).mouseup(function (e) {
     closeNav();
   }
 });
+
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
